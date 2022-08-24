29e2a86e4ac0d3a866d3e5cdc14ef57f616a0f45
jreyes1990 <jarl1990mt@gmail.com>
2022-08-23 23:17:53 -0600

    Desarrollo nuevo core en rails 6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,68 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "startbootstrap-sb-admin-2/vendor/jquery/jquery";
+import "@popperjs/core/dist/umd/popper";
+import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle";
+import "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing";
+import "startbootstrap-sb-admin-2/js/sb-admin-2";
+
+import "startbootstrap-sb-admin-2/vendor/fontawesome-free/js/all";
+
+
+document.addEventListener("turbolinks:load", () => {
+    /* *******************************************************
+      * Para controlar el sidebar en posición cerrado o abierto
+      * ******************************************************** */
+    let sidebarState = sessionStorage.getItem('sidebar');
+    $(".sidebar").toggleClass(sidebarState);
+
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+            sessionStorage.setItem('sidebar', 'toggled');
+            $('.sidebar .collapse').collapse('hide');
+        } else {
+            sessionStorage.setItem('sidebar', '');
+        }
+    });
+
+    // Cierre cualquier acordeón de menú abierto cuando la ventana cambie de tamaño a continuación 768px
+    if ($(window).width() < 768) {
+        $('.sidebar .collapse').collapse('hide');
+    }
+
+    // Cierre cualquier acordeón de menú abierto cuando la ventana cambie de tamaño a continuación 480px
+    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+        $("body").addClass("sidebar-toggled");
+        $(".sidebar").addClass("toggled");
+        $('.sidebar .collapse').collapse('hide');
+    }
+
+    if (sidebarState === 'toggled') {
+        $(".menu_sb").addClass("collapsed");
+        $(".opcion_sb").removeClass("show");
+    }
+    /* *******************************************************
+     * Fin para controlar el sidebar en posición cerrado o abierto
+     * ******************************************************** */
+
+    // Configuracion de Scroll Top Button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('#topBtn').fadeIn();
+        } else {
+            $('#topBtn').fadeOut();
+        }
+    });
+
+    $("#topBtn").click(function () {
+        $('html ,body').animate({
+            scrollTop: 0
+        }, 800)
+    });
+
+
+});
