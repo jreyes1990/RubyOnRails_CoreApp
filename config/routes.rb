0115038ad5212bf2ca5fb53e80d49cf4dc54cfb6
jreyes1990 <jarl1990mt@gmail.com>
2022-09-04 19:28:57 -0600

    Se agrego la ruta de Persona.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  scope "/sistemas" do
+    #Manejo controller Personas
+    resources :personas
+    #patch 'personas/update'
+    get 'personas/show/:id' => "personas#show", as: 'ver_perfil'
+    get 'inactivar/:id' => "personas#inactivar_persona", as: 'inactivar_persona'
+  end
+
   scope "/iconos" do
     #Manejo de controller font_awesome
     resources :font_awesomes
