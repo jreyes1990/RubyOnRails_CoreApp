262e4a228d8dd174569a5f9abc53a6970439697c
jreyes1990 <jarl1990mt@gmail.com>
2022-09-18 22:55:48 -0600

    Se agregaron algunas inflecciones del modelo.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,10 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'RESTful'
+  inflect.irregular "opcion", "opciones"
+  inflect.irregular "rol", "roles"
+  inflect.irregular "menu_rol", "menu_roles"
+end
